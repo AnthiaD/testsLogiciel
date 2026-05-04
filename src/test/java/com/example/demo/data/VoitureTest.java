af0f3538da3becc998964056f4718cef1f7292f9
Nthiaa17 <dialloanthia03@gmail.com>
2026-05-04 14:22:18 +0200

    Ajout des tests Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -5,13 +5,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-// test pull request
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        Voiture voiture = new Voiture("Ferrari", 5000);
+        assertEquals("Ferrari", voiture.getMarque());
+        assertEquals(5000, voiture.getPrix());
+
+    }
+    @Test
+    void modifierVoiture(){
+        Voiture voiture = new Voiture();
+
+        voiture.setId(1);
+        voiture.setMarque("Mercedes");
+        voiture.setPrix(10000);
+
+        assertEquals("Mercedes", voiture.getMarque());
+        assertEquals(10000, voiture.getPrix());
+        assertEquals(1, voiture.getId());
+    }
+
+    @Test
+    void TestToString(){
+        Voiture voiture = new Voiture("Mercedes", 10000);
+        voiture.setId(1);
+
+        String test = voiture.toString();
+
+        assertTrue(test.contains("Marque: Mercedes"));
+        assertTrue(test.contains("Prix: 10000"));
+        assertTrue(test.contains("id: 1"));
     }
 
 }
