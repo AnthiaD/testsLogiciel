acb16c72a6415b36b46760b7c51394b0c94af518
Nthiaa17 <dialloanthia03@gmail.com>
2026-05-11 14:18:24 +0200

    Update StatistiqueTests.java
    Ajout des tests jUnit et Mockito

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
 
 @SpringBootTest
 public class StatistiqueTests {
@@ -14,4 +14,26 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    void ajouterVoiture() {
+        Voiture voiture = new Voiture("Ferrari", 5000);
+
+        statistiqueImpl.ajouter(voiture);
+
+        verify(statistiqueImpl, times(1)).ajouter(voiture);
+    }
+
+    @Test
+    void calculerPrixMoyen() {
+        Echantillon echantillon = new Echantillon(2, 7500);
+
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillon);
+
+        Echantillon resultat = statistiqueImpl.prixMoyen();
+
+        assertEquals(2, resultat.getNombreDeVoitures());
+        assertEquals(7500, resultat.getPrixMoyen());
+
+        verify(statistiqueImpl, times(1)).prixMoyen();
+    }
 }
